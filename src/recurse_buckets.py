e30ba0346bcc00fa21cc0c387b9a7ffc79f1096f
Matthew Mangione <matthewmmangione@gmail.com>
2022-01-25 15:07:05 -0500

    added creating a file

--- src/recurse_buckets.py
+++ src/recurse_buckets.py
@@ -10,20 +10,25 @@ def list_buckets():
         response = s3_client.list_buckets(NextToken=response["NextToken"])
         bucket_list.extend(response["Buckets"])
 
-    for bucket in bucket_list:
-        bucket_name = bucket['Name']
-        print(f"Bucket Name: {bucket_name}")
-        # {'Name': 'builds-astech-io', 'CreationDate': datetime.datetime(2020, 9, 7, 3, 44, 18, tzinfo=tzutc())}
-        paginator = s3_client.get_paginator('list_objects')
-        for result in paginator.paginate(Bucket=f'{bucket_name}', Delimiter='/'):
-            prefix_list = []
-            prefix_list = result.get('CommonPrefixes')
-            if prefix_list:
-                print("prefix list:")
-                for prefix in prefix_list:
-                    print(f"    {prefix['Prefix']}")
-            else:
-                print(f"Bucket {bucket_name} doesn't have folders")
+    # with open('xmen.txt', 'w+') as my_file:
+    with open('bucket_and_prefixes.txt', 'w') as bucketfile:
+        for bucket in bucket_list:
+            bucket_name = bucket['Name']
+            print(f"Bucket Name: {bucket_name}")
+            bucketfile.write(f"Bucket name: {bucket_name}\n")
+            paginator = s3_client.get_paginator('list_objects')
+            for result in paginator.paginate(Bucket=f'{bucket_name}', Delimiter='/'):
+                prefix_list = []
+                prefix_list = result.get('CommonPrefixes')
+                if prefix_list:
+                    print("prefix list:")
+                    bucketfile.write(f"prefix list:\n")
+                    for prefix in prefix_list:
+                        bucketfile.write(f"    {prefix['Prefix']}\n")
+                        print(f"    {prefix['Prefix']}")
+                else:
+                    print(f"Bucket {bucket_name} doesn't have folders")
+                    bucketfile.write(f"    Bucket {bucket_name} doesn't have folders\n")
         print()
 
 if __name__ == "__main__":
